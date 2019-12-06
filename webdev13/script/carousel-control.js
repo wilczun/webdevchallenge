fee703ad9f280e6403fdefaf84a01e005d7c794d
Paul <56404443+wilczun@users.noreply.github.com>
2019-12-06 07:08:35 +0100

    content added
    testimonials, contact, footer added

---
+++ webdev13/script/carousel-control.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+  center: true,
+  loop:true,
+  margin:50,
+  autoWidth:true,
+  nav:false,
+  responsive:{
+      0:{
+          items:3
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:3
+      }
+  }
+})
