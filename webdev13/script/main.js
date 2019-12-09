30b7a7946ef840a0c41d04faef1ad9ef166e72b3
Paul <56404443+wilczun@users.noreply.github.com>
2019-12-09 02:20:36 +0100

    navbar update

--- webdev13/script/main.js
+++ webdev13/script/main.js
@@ -1,23 +1,18 @@
-window.onscroll = function() {myFunction()};
 window.onscroll = function() {changeHeight()};
 window.onscroll = function() {myFunction()};
 
 // Get the navbar
 var navbar = document.getElementById("navbar-main");
-var headerText = document.getElementById("header-content")
 
 // Get the offset position of the navbar
 var sticky = navbar.offsetTop;
-var headerMargin = headerText.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
   if (window.pageYOffset > sticky) {
-    navbar.classList.add("sticky")
-    headerText.classList.add("change-height")
+    navbar.classList.add("menu-bg")
   } else {
-    navbar.classList.remove("sticky");
-    headerText.classList.remove("change-height");
+    navbar.classList.remove("menu-bg");
   }
 }
 
